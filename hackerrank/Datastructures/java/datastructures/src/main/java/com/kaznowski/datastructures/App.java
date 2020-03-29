893926faec11f70f81d0d9ad5cbe02ef16eaed30
Hugh Kaznowski <phkaznowski@gmail.com>
2020-03-29 21:14:13 +0100

    Datastructures hackerrank java

---
+++ hackerrank/Datastructures/java/datastructures/src/main/java/com/kaznowski/datastructures/App.java
@@ -0,0 +1,13 @@
+package com.kaznowski.datastructures;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
